d465f1d5aedab68996a81ed0c9bce88561cce677
Tom Elliott <ipse@paregorios.org>
2023-04-12 16:41:07 -0500

    header validation for Gazetteer

--- src/pleiades_python/gazetteer.py
+++ src/pleiades_python/gazetteer.py
@@ -9,11 +9,65 @@
 Defines Gazetteer class, which wraps all interaction with the web application
 """
 
+from importlib.metadata import version
 import logging
+from webiquette.webi import Webi
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_USER_AGENT = f"PleiadesPython/{version('pleiades_python')}"
+
 
 class Gazetteer:
-    def __init__(self):
-        pass
+    def __init__(self, user_agent: str = DEFAULT_USER_AGENT, headers: dict = {}):
+        self.headers = self._validate_headers(headers)
+        ua = user_agent
+        if not ua or ua == DEFAULT_USER_AGENT:
+            try:
+                ua = self.headers["User-Agent"]
+            except KeyError:
+                pass
+        self.headers["User-Agent"] = self._validate_user_agent(ua)
+
+    def _validate_headers(self, headers: dict) -> dict:
+        """Validate any custom headers added by the user"""
+        clean_headers = dict()
+        for k, v in headers.items():
+            if k not in ["From", "Referer", "User-Agent"]:
+                logger.error(f"Unsupported HTTP request header '{k}' suppressed.")
+                continue
+            if not isinstance(v, str):
+                raise TypeError(
+                    f"Unexpected type for custom HTTP request header '{k}'."
+                    f"Expected str, got {type(v)}"
+                )
+            value = v.strip()
+            if not value:
+                raise KeyError(
+                    "Unacceptable key for custom HTTP request header "
+                    f"'{k}'. Expected a non-zero-length string, got ''."
+                )
+            clean_headers[k] = value
+        return clean_headers
+
+    def _validate_user_agent(self, user_agent: str) -> str:
+        """Ensure the user agent is a non-zero-length string"""
+        if not isinstance(user_agent, str):
+            raise TypeError(
+                "Unexpected type for user_agent parameter. Expected str, "
+                f"got {type(user_agent)}"
+            )
+        ua = user_agent.strip()
+        if ua == DEFAULT_USER_AGENT:
+            logger.warning(
+                f"Using default user-agent ({user_agent}) for all requests "
+                "to the Pleiades gazetteer website. We strongly prefer you "
+                "define your own unique user-agent string and pass it to "
+                "the Gazetteer class at instantiation."
+            )
+        elif not ua:
+            raise ValueError(
+                f"Unacceptable value for user_agent parameter. Expected a "
+                "non-zero-length string, got ''."
+            )
+        return ua
--- tests/test_gazetteer.py
+++ tests/test_gazetteer.py
@@ -9,8 +9,7 @@
 Test pleiades_python.gazetteer module
 """
 
-from datetime import datetime
-from pleiades_python.gazetteer import Gazetteer
+from pleiades_python.gazetteer import Gazetteer, DEFAULT_USER_AGENT
 from pathlib import Path
 from pytest import raises
 from shutil import rmtree
@@ -32,5 +31,34 @@ class TestGazetteer:
     def teardown_class(cls):
         pass
 
-    def test_init(self):
-        pass
+    def test_init_defaults(self):
+        g = Gazetteer()
+        h = g.headers
+        assert isinstance(h, dict)
+        assert len(h) == 1
+        assert h["User-Agent"] == DEFAULT_USER_AGENT
+
+    def test_init_user_agent(self):
+        s = "SuperPleiadesFunBot/8675309"
+        g = Gazetteer(user_agent=s)
+        assert g.headers["User-Agent"] == s
+
+    def test_init_user_agent_in_header(self):
+        s = "SuperPleiadesFunBot/8675309"
+        g = Gazetteer(headers={"User-Agent": s})
+        assert g.headers["User-Agent"] == s
+
+    def test_init_user_agent_override(self):
+        s = "SuperPleiadesFunBot/8675309"
+        g = Gazetteer(user_agent=s, headers={"User-Agent": "PleiadesFishBot/42"})
+        assert g.headers["User-Agent"] == s
+
+    def test_init_custom_header(self):
+        m = "marvin@end.universe.restaurant"
+        g = Gazetteer(headers={"From": m})
+        assert g.headers["From"] == m
+
+    def test_init_suppress_unsupported_header(self):
+        g = Gazetteer(headers={"Vorpal": "bunny"})
+        with raises(KeyError):
+            g.headers["Vorpal"]
